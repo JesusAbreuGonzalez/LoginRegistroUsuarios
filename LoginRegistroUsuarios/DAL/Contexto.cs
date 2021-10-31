830f30427b3824f306604829e89f94d2ecc8abaa
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-10-30 21:01:32 -0400

    Creando base de datos

---
+++ LoginRegistroUsuarios/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using LoginRegistroUsuarios.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginRegistroUsuarios.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data source = Data/GestionUsuariosDB.db");
+        }
+    }
+}
---
+++ LoginRegistroUsuarios/Models/Usuarios.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginRegistroUsuarios.Models
+{
+    public class Usuarios
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public Usuarios()
+        {
+            UsuarioId = 0;
+            Username = string.Empty;
+            Password = string.Empty;
+        }
+    }
+}
